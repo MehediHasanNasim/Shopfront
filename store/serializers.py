00bd4b00e8d76f7bd4a3abbeba82777b56ca1f2d
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-07-04 01:20:42 +0600

    Modelserializer & Deserializer

--- store/serializers.py
+++ store/serializers.py
@@ -3,19 +3,48 @@ from rest_framework import serializers
 from .models import *
 from .import views
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+'''model serializer'''
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=10, decimal_places=2, source='unit_price')
+
+'''Serializer'''
+# class CollectionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+
+
+'''model serializer'''
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+    
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name = 'collection-detail',
-    )
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
+    
+
+''' validation example'''
+    # def validate(self, data):
+    #     if data['password'] != data['confirm_password']:
+    #         return serializers.ValidationError('Password do not match')
+    #     return data
+    
+
+'''Serializer'''
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=10, decimal_places=2, source='unit_price')
+#     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+#     collection = serializers.HyperlinkedRelatedField(
+#         queryset = Collection.objects.all(),
+#         view_name = 'collection-detail',
+#     )
     # # For whole of the fields
     # collection = CollectionSerializer()
 
@@ -25,8 +54,8 @@ class ProductSerializer(serializers.Serializer):
     #     queryset = Collection.objects.all()
     # )
 
-    def calculate_tax(self, product: Product):
-        return product.unit_price * Decimal(1.1)
+    # def calculate_tax(self, product: Product):
+    #     return product.unit_price * Decimal(1.1)
     
     # price = serializers.DecimalField(coerce_to_string=False)
 
--- store/views.py
+++ store/views.py
@@ -3,15 +3,21 @@ from django.http import HttpResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import *
 from .serializers import *
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True, context={'request':request})
-    return Response(serializer.data)
-
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('ok')
 
 @api_view()
 def product_detail(request, id):
