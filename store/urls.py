b6c181ad230bbbe631c671141963606119e184b2
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-08-08 19:55:23 +0600

    adding Cart api, update & delete functionality

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,6 @@ from .import views
 # from pprint import pprint
 
 
-
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
@@ -17,7 +16,11 @@ router.register('carts', views.CartViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
-urlpatterns = router.urls + products_router.urls 
+
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items-detail')
+
+urlpatterns = router.urls + products_router.urls + carts_router.urls
 
 # urlpatterns = [
 #     path('', include(router.urls)),
--- store/views.py
+++ store/views.py
@@ -78,6 +78,26 @@ class CartViewSet(CreateModelMixin,
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
 
+
+class CartItemViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+        
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
+    
+    def get_queryset(self):
+        return CartItem.objects \
+                .filter(cart_id=self.kwargs['cart_pk']) \
+                .select_related('product')
+    
+
 # class CartViewSet(ModelViewSet):
 #     queryset = Cart.objects.prefetch_related('items__product').all()
 #     serializer_class = CartSerializer
